1fdf069b2805cab78228f71a8fb04ce34ae6ca73
sumer007 <sadawartisumer@gmail.com>
2015-10-17 16:03:01 +0530

    ANN, Classification and SVM were added

---
+++ src/ml/supervised/classification.py
@@ -0,0 +1,18 @@
+from __future__ import division
+import numpy as np
+import os
+
+class ANN:
+    """ Artificial Neural Network used for classification"""
+    def __init__(self):
+        return
+    
+class Classification:
+    """ Linear Classification Algorithm """
+    def __init__(self):
+        return
+    
+class SVM:
+    """ Support Vector Machines """
+    def __init__(self):
+        return
